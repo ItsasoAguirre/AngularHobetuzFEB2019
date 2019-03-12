7058702fa60d77fd6814f6ffd751ea2d670c848b
hobetuz <hobetuz@ipartekaula.com>
2019-03-12 20:26:46 +0100

    Añadir formulario a equipos

--- equipos/src/app/equipo/equipo.component.ts
+++ equipos/src/app/equipo/equipo.component.ts
@@ -2,25 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { Equipo } from './equipo';
 import es from '@angular/common/locales/es';
 import {registerLocaleData} from '@angular/common';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-equipo',
   templateUrl: './equipo.component.html',
   styleUrls: ['./equipo.component.css']
+
 })
 export class EquipoComponent implements OnInit {
   equipos: Array<Equipo>;
   nuevoEquipo: Equipo;
   searchText: string;
   equipoSeleccionado: Equipo;
-  constructor() { }
+  formGroup: FormGroup;
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
     registerLocaleData(es);
+    this.buildForm();
     this.equipos = [];
     this.nuevoEquipo = new Equipo('', '', 0, 0, '');
     this.searchText = '';
   }
+
+  private buildForm() {
+    const fundacion = 0;
+    const nombre = '';
+    this.formGroup = this.formBuilder.group({
+
+      nombre: '',
+      fundacion: 0,
+      escudo: '',
+      socios: 0,
+      ciudad: ''
+
+    });
+  }
+
 
   addToArray() {
     this.equipos.push(this.nuevoEquipo);
